1ecafdfe381cee455cb9a2ecceef67dd312f529b
a52cents <96891277+a52cents@users.noreply.github.com>
2023-11-02 06:44:16 +0100

    Update Update.jsx

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,60 +1,44 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './Add.css';
-
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import './Add.css'
 const Update = () => {
-  const [car, setCar] = useState({
-    title: '',
-    description: '',
-    price: null,
-    cover: null, // Modifier pour gérer le fichier image
-  });
 
-  const navigate = useNavigate();
-  const location = useLocation();
-  const carId = location.pathname.split('/')[2];
+    const [car, setCar] = useState({
+        title: '',
+        description: '',
+        price: null,
+        cover: ''
 
-  const handleChange = (e) => {
-    if (e.target.name === 'cover') {
-      // Pour gérer le changement de fichier image
-      setCar({ ...car, [e.target.name]: e.target.files[0] });
-    } else {
-      setCar((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    })
+
+    const navigate = useNavigate()    
+
+    const handleChange = (e) => {
+        setCar((prev) => ({...prev, [e.target.name]: e.target.value}))
     }
-  };
-
-  const handleClick = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('title', car.title);
-    formData.append('description', car.description);
-    formData.append('price', car.price);
-    formData.append('image', car.cover); // Ajout du fichier image
-
-    try {
-      await axios.put(`https://car-portfolio.onrender.com/cars/${carId}`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      navigate('/');
-    } catch (err) {
-      console.log(err);
+    
+    const handleClick = async e => {
+        e.preventDefault()
+        try{
+            await axios.put("https://car-portfolio.onrender.com/cars", car)
+            navigate("/")
+        }catch(err){
+            console.log(err)
+        }
     }
-  };
-
+    console.log(car)
   return (
-    <div className="form">
-      <h1>Update this car</h1>
-      <input type="text" placeholder="title" onChange={handleChange} name="title" />
-      <input type="text" placeholder="description" onChange={handleChange} name="description" />
-      <input type="number" placeholder="price" onChange={handleChange} name="price" />
-      <input type="file" onChange={handleChange} name="cover" />
+    <div className='form'>
+        <h1>Add a new car</h1>
+        <input type="text" placeholder='title' onChange={handleChange} name="title" />
+        <input type="text" placeholder='description' onChange={handleChange} name='description' />
+        <input type="number" placeholder='price' onChange={handleChange} name='price' />
+        <input type="file" onChange={handleChange} name='cover'/>
 
-      <button onClick={handleClick}>Update</button>
+        <button onClick={handleClick}>Add</button>
     </div>
-  );
-};
+  )
+}
 
-export default Update;
+export default Update
